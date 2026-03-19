a37ce85fde02fdf609924ce4def81ed29592b8bd
Elissa <ecimino@umich.edu>
2026-03-19 14:21:11 -0400

    update gyrodometry function

--- src/mbot_odometry.c
+++ src/mbot_odometry.c
@@ -17,5 +17,32 @@ void mbot_calculate_odometry(float vx, float vy, float wz, float dt, float* x, f
 }
 
 void mbot_calculate_gyrodometry(float vx, float vy, float wz, float dt, float gyro_z, float* x, float* y, float* theta) {
+    float dtheta_gyro = gyro_z * dt;
+    float dtheta_odom = wz * dt;
+
+    float dG_O = dtheta_gyro - dtheta_odom;
+
+    float dtheta_threshold = 0.125 * (PI/180) * 0.1;
+
+    float abs_GO = fabs(dG_O);
+
+    float theta_curr = 0; // Initialize theta_curr
+
+    if (abs_GO > dtheta_threshold){
+        theta_curr = *theta + dtheta_gyro * dt;
+    } else if (abs_GO < dtheta_threshold){
+        theta_curr = *theta + dtheta_odom * dt;
+    }
+
+    // Update pose
+    *x += vx * dt * cos(theta_curr) - vy * dt * sin(theta_curr);
+    *y += vx * dt * sin(theta_curr) + vy * dt * cos(theta_curr);
+    // *theta += wz * dt;
+    *theta = theta_curr;
+
+    // Normalize theta to [-pi, pi]
+    while (*theta > PI) *theta -= 2.0 * PI;
+    while (*theta <= -PI) *theta += 2.0 * PI;
+
     return;
 }
